7d58a3af7d4cd53bdb1eb7a7b39b19fc49de2842
JoelEscobar13 <joelescobar216@gmail.com>
2025-02-08 17:29:21 -0400

    Comprobando 4
    comprobando4

--- frontend/src/components/MainApp.jsx
+++ frontend/src/components/MainApp.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import Camera from "./Camera";
-import "./MainApp.css";
 import VoiceInterface from "./VoiceInterface";
+import "./MainApp.css";
 
 const App = () => {
     return (
